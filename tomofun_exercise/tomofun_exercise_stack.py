ac3538cbe234d64c210292c26c030ffe5891a60b
vladka_huang <sunsky11881@gmail.com>
2024-09-21 21:21:26 +0800

    modify cdk stack

--- tomofun_exercise/tomofun_exercise_stack.py
+++ tomofun_exercise/tomofun_exercise_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_sqs as sqs,
+    aws_lambda as _lambda,
+    Duration,
 )
 from constructs import Construct
 
@@ -10,10 +11,34 @@ class TomofunExerciseStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create SQS queue
+        queue = sqs.Queue(
+            self, "TomofunQueue",
+            visibility_timeout=Duration.seconds(300)
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "TomofunExerciseQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create Lambda function for POST request
+        post_lambda = _lambda.Function(
+            self, "PostLambda",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="post_lambda.handler",
+            code=_lambda.Code.from_asset("lambda"),
+            environment={
+                "QUEUE_URL": queue.queue_url
+            }
+        )
+
+        # Create Lambda function for GET request
+        get_lambda = _lambda.Function(
+            self, "GetLambda",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="get_lambda.handler",
+            code=_lambda.Code.from_asset("lambda"),
+            environment={
+                "QUEUE_URL": queue.queue_url
+            }
+        )
+
+        # Grant permissions to Lambda functions
+        queue.grant_send_messages(post_lambda)
+        queue.grant_consume_messages(get_lambda)
